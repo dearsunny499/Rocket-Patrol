e0dd442d4ca589f86599d8fa193ccd9b6b55de9a
Yanyao <yhu214@ucsc.edu>
2021-06-26 18:08:13 +0000

    create score keeping

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -47,6 +47,25 @@ class Play extends Phaser.Scene {
 			frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
 			frameRate: 30
 		});
+		
+		// initialize score
+		this.p1Score = 0;
+		
+		// display score
+		let scoreConfig = {
+			fontFamily: 'Courier',
+			fontSize: '28px',
+			backgroundColor: '#F3B141',
+			color: '#843605',
+			align: 'right',
+			padding: {
+				top: 5,
+				bottom: 5,
+			},
+			fixedWidth: 100
+		}
+		this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+		
 	}
 	update() {
 		this.starfield.tilePositionX -= 4;
@@ -69,7 +88,6 @@ class Play extends Phaser.Scene {
 		  this.p1Rocket.reset();
 		  this.shipExplode(this.ship01);
 		}
-		
 	}
 	checkCollision(rocket, ship) {
 		// simple AABB checking
@@ -84,14 +102,17 @@ class Play extends Phaser.Scene {
 	}
 	shipExplode(ship) {
 		// temporarily hide ship
-		ship.alpha = 0;
+		ship.alpha = 0;                         
 		// create explosion sprite at ship's position
 		let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
 		boom.anims.play('explode');             // play explode animation
-		boom.on('animationcomplete', () => {    // callback after anim completes
-		ship.reset();                         // reset ship position
-		ship.alpha = 1;                       // make ship visible again
-		boom.destroy();                       // remove explosion sprite
-		});       
+		boom.on('animationcomplete', () => {    // callback after ani completes
+			ship.reset();                       // reset ship position
+			ship.alpha = 1;                     // make ship visible again
+			boom.destroy();                     // remove explosion sprite
+		});
+		// score add and repaint
+		this.p1Score += ship.points;
+		this.scoreLeft.text = this.p1Score;       
 	}
 }

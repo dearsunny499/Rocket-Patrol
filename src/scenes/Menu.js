e6159c646f3818b1218a96b664f3b2bfc185a20e
Yanyao <yhu214@ucsc.edu>
2021-06-26 17:18:29 +0000

    fixes a bug

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -3,16 +3,6 @@ class Menu extends Phaser.Scene {
 		super("menuScene");
 	}
 	create() {
-		// set UI sizes
-		let borderUISize = game.config.height / 15;
-		let borderPadding = borderUISize / 3;
-		
-		// green UI background
-		this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
-		// white borders
-		this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-		this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
-		this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-		this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
+		this.scene.start("playScene");
 	}
 }
